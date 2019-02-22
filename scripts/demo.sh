304833aa2bf9fd5bc58e0d8e38ad48e46d1cf532
dingyujian <dingyujian@dingyujiandeMacBook-Pro.local>
2019-02-22 18:16:38 +0800

    modify scripts/demo.sh

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -9,11 +9,11 @@ fi
 for campaign in $campaigns; do
     echo $campaign
     # run logistc regression
-    python python/lryzx.py $folder/$campaign/train.yzx.txt $folder/$campaign/test.yzx.txt
+    python3 python/lryzx.py $folder/$campaign/train.yzx.txt $folder/$campaign/test.yzx.txt
     
     # run real-time bidding test
-    python python/rtb-test.py $folder/$campaign/train.yzx.txt $folder/$campaign/test.yzx.txt $folder/$campaign/test.yzx.txt.lr.pred $resultsfolder/rtb.results.$campaign.tsv
+    python3 python/rtb-test.py $folder/$campaign/train.yzx.txt $folder/$campaign/test.yzx.txt $folder/$campaign/test.yzx.txt.lr.pred $resultsfolder/rtb.results.$campaign.tsv
     
     # check the best perf
-    python python/check-best-perf.py $resultsfolder/rtb.results.$campaign.tsv
+    python3 python/check-best-perf.py $resultsfolder/rtb.results.$campaign.tsv
 done
