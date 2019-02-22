38da77f5ee1efdd1a8719eaae1b4fb86c487622c
dingyujian <dingyujian@dingyujiandeMacBook-Pro.local>
2019-02-22 17:40:24 +0800

    modify path

--- scripts/demo.sh
+++ scripts/demo.sh
@@ -1,6 +1,6 @@
 campaigns="1458"
-folder=../../make-ipinyou-data
-resultsfolder=../results
+folder=../make-ipinyou-data
+resultsfolder=results
 
 if [ ! -d $resultsfolder ]; then
   mkdir $resultsfolder
@@ -9,11 +9,11 @@ fi
 for campaign in $campaigns; do
     echo $campaign
     # run logistc regression
-    python ../python/lryzx.py $folder/$campaign/train.yzx.txt $folder/$campaign/test.yzx.txt
+    python python/lryzx.py $folder/$campaign/train.yzx.txt $folder/$campaign/test.yzx.txt
     
     # run real-time bidding test
-    python ../python/rtb-test.py $folder/$campaign/train.yzx.txt $folder/$campaign/test.yzx.txt $folder/$campaign/test.yzx.txt.lr.pred $resultsfolder/rtb.results.$campaign.tsv
+    python python/rtb-test.py $folder/$campaign/train.yzx.txt $folder/$campaign/test.yzx.txt $folder/$campaign/test.yzx.txt.lr.pred $resultsfolder/rtb.results.$campaign.tsv
     
     # check the best perf
-    python ../python/check-best-perf.py $resultsfolder/rtb.results.$campaign.tsv
+    python python/check-best-perf.py $resultsfolder/rtb.results.$campaign.tsv
 done
